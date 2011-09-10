11067fa2c2e93d4bc73acf2a7706a74dffef3d95
Martin C. Doege <mdoege@compuserve.com>
2011-09-10 19:54:43 +0200

    added matching and improved warnings script

--- biowarn.py
+++ biowarn.py
@@ -4,7 +4,7 @@ P: physical
 E: emotional
 I: intellectual
 
-2011-09-02
+2011-09-09
 
 Red: critical days
 Orange: mini-critical days
@@ -27,24 +27,39 @@ wa=(
 ((1,17,18),(9,26))
 )
 
-s = {'_': '<font color="#cccccc">&#9679;</font>', 'y': '<font color="#ffd700">&#9679;</font>', 'r': '<font color="#ff0000">&#9679;</font>'}
+tag = ('Mo.','Di.','Mi.','Do.','Fr.','Sa.','So.')
 
-print "<html><head><title>%s</title><body><h2>%s</h2>" % (ti, ti)
+s = {'_': '<font color="#00ff00">&#9679;</font>', 'y': '<font color="#ffd700">&#9679;</font>', 'r': '<font color="#ff0000">&#9679;</font>'}
+
+print "<html><head><title>%s</title><body><h3>%s</h3>" % (ti, ti)
 
 print '<font face="Courier New">'
 
-for t in range(t1-3,t1+41):
+for t in range(t1-3,t1+35):
+	if not (date.fromordinal(t).day-1)%5 and (date.fromordinal(t).day-1)%30:
+		print '<hr width="42%" align="left">'
+	if date.fromordinal(t).day == 1:
+		print '<hr width="60%" align="left">'
 	w = ['_','_','_']
+	o = ['*','*','*']
 	for c in range(3):
 		p = 23+5*c
 		v = ((t-t0) % p)+1
+		if (v-1) <= p/2:
+			o[c] = 'H'
+		if (v-1) >= p/2:
+			o[c] = 'T'
 		if v in wa[c][0]:
 			w[c] = 'r'
+			o[c] = 'K'
 		if v in wa[c][1]:
 			w[c] = 'y'
-	print date.fromordinal(t)
+	a = date.fromordinal(t)
+	print tag[a.weekday()], a
 	for x in w:
 		print s[x]
+	for x in o:
+		print x,
 	print "<br>"
 
 print "</font></body></html>"
